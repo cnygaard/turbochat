fb3644190446c8c1ece03ba3c5aec955aff672e6
Christian Nygaard <christiannygaard@Christians-MacBook.local>
2013-07-01 23:33:07 +0200

    Fixed the time bug

--- chat.js
+++ chat.js
@@ -8,34 +8,38 @@ if (Meteor.isClient)
       return Messages.find({}, { sort: { time: 1 }});
     }
 
-Template.entryfield.events = {
-  "keydown #message": function(event){
-    if(event.which == 13){
-      // Submit the form
-      var name = document.getElementById('name');
-      var message = document.getElementById('message');
-      var file = document.getElementById('file');
-      var date = new Date(Date.now());
-      // hours part from the timestamp
-      var hours = date.getHours();
-      // minutes part from the timestamp
-      var minutes = date.getMinutes();
+  Template.entryfield.events = 
+  {
+    "keydown #message": function(event)
+    {
+      if(event.which == 13)
+      {
+        // Submit the form
+        var name = document.getElementById('name');
+        var message = document.getElementById('message');
+        var file = document.getElementById('file');
+        var date = new Date(Date.now());
+        // hours part from the timestamp
+        var hours = date.getHours().toString();
+        // minutes part from the timestamp
+        var minutes = date.getMinutes();
 
-      if(name.value != '' && message.value != ''){
-        Messages.insert({
-          name: name.value,
-          message: message.value,
-          time: Date.now(),
-          hours: hours,
-          minutes: minutes
-        });
+        if(name.value != '' && message.value != '')
+        {
+          Messages.insert({
+            name: name.value,
+            message: message.value,
+            time: Date.now(),
+            hours: hours,
+            minutes: minutes
+          });
 
-        //name.value = '';
-        message.value = '';
+          //name.value = '';
+          message.value = '';
+        }
       }
     }
   }
-}
 }
 
 if (Meteor.isServer) {
